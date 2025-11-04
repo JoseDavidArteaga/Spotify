c259ec259ea2cd4f9cc98ff3ccccbd7074e3776d
JoseDavidArteaga <josedavidart@unicauca.edu.co>
2025-11-04 02:09:06 -0500

    Inicio

---
+++ microservicioEnvioCorreos - plantilla/src/main/java/co/edu/unicauca/infoii/correo/DTOs/CancionAlmacenarDTOInput.java
@@ -0,0 +1,12 @@
+package co.edu.unicauca.infoii.correo.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CancionAlmacenarDTOInput {	
+}
+	
---
+++ microservicioEnvioCorreos - plantilla/src/main/java/co/edu/unicauca/infoii/correo/componenteRecibirMensajes/MessageConsumer.java
@@ -0,0 +1,15 @@
+package co.edu.unicauca.infoii.correo.componenteRecibirMensajes;
+
+import org.springframework.stereotype.Service;
+
+import co.edu.unicauca.infoii.correo.DTOs.CancionAlmacenarDTOInput;
+import co.edu.unicauca.infoii.correo.commons.Simulacion;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+@Service
+public class MessageConsumer {
+
+    
+}
+    
